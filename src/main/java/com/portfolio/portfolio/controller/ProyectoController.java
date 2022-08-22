2cb971b95a9ecd3f4b20c14f8b632d1ae5b4f112
Sophia Alvarez <s.alvarez.bulfon@gmail.com>
2022-08-22 15:11:30 -0300

    CRUD Proyectos

--- src/main/java/com/portfolio/portfolio/controller/ProyectoController.java
+++ src/main/java/com/portfolio/portfolio/controller/ProyectoController.java
@@ -1,6 +1,7 @@
 package com.portfolio.portfolio.controller;
 
 import com.portfolio.portfolio.dto.Mensaje;
+import com.portfolio.portfolio.dto.ProyectoDTO;
 import com.portfolio.portfolio.entity.Proyecto;
 import com.portfolio.portfolio.service.ProyectoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +25,32 @@ public class ProyectoController {
     }
 
 
-    @GetMapping("/detalles/(id)")
+    @GetMapping("/details/(id)")
     public  ResponseEntity<Proyecto> getById(@PathVariable("id") long id){
         if(!proyectoService.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
         }
         Proyecto proyecto = proyectoService.getOne(id).get();
         return  new ResponseEntity<Proyecto>(proyecto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProject(@PathVariable("id") Long id){
+        if(!proyectoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No hay"), HttpStatus.NOT_FOUND);
+        proyectoService.deleteProjects(id);
+        return new ResponseEntity(new Mensaje("Eliminado"), HttpStatus.OK);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<?> editProject(@PathVariable("id") Long id,
+                                         @RequestBody ProyectoDTO projectDTO){
+        Proyecto proyecto = proyectoService.getOne(id).get();
+        proyecto.setName(projectDTO.getName());
+        proyecto.setDescription(projectDTO.getDescription());
+        proyecto.setUrl_image(projectDTO.getUrl_image());
+        proyecto.setUrl_sourceCode(projectDTO.getUrl_sourceCode());
+        proyectoService.saveProjects(proyecto);
+        return new ResponseEntity(new Mensaje("Editado"), HttpStatus.OK);
     }
 }
