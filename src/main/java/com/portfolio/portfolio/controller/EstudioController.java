1a6f46495475f28cbfea6da9093d3acd26e7916c
Sophia Alvarez <s.alvarez.bulfon@gmail.com>
2022-08-26 22:52:10 -0300

    Firebase owo

--- src/main/java/com/portfolio/portfolio/controller/EstudioController.java
+++ src/main/java/com/portfolio/portfolio/controller/EstudioController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/estudio")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class EstudioController {
     @Autowired
     EstudioService estudioService;
--- src/main/java/com/portfolio/portfolio/controller/ProyectoController.java
+++ src/main/java/com/portfolio/portfolio/controller/ProyectoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class ProyectoController {
     @Autowired
     ProyectoService proyectoService;
