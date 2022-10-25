80100ca20b663f66118ca30a541a237cfe8aaf15
Sophia Alvarez <101871038+SAlvarezBulfon@users.noreply.github.com>
2022-10-25 20:37:50 -0300

    uwu

--- src/main/java/com/portfolio/portfolio/entity/Experiencia.java
+++ src/main/java/com/portfolio/portfolio/entity/Experiencia.java
@@ -26,11 +26,11 @@ public class Experiencia {
 
     //Getters y setters
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
